d384608ba6629b4ed6615aac1cdea3d5d7f19e5b
Yannick Wolff <yannickw@sicara.com>
2024-01-25 09:16:32 +0100

    [Question 15] Serve model with FastAPI

--- solution/README.md
+++ solution/README.md
@@ -17,3 +17,15 @@ To see script usage, run:
 ```bash
 python train_model.py --help
 ```
+
+## Run model serving API
+
+```bash
+uvicorn serving:app --reload
+```
+
+## Request model serving API
+
+```bash
+curl -X POST http://127.0.0.1:8000/predict -F "file=@data/test_image.jpg"
+```
--- solution/serving.py
+++ solution/serving.py
@@ -1,3 +1,4 @@
+from fastapi import FastAPI, UploadFile
 from torchvision import datasets, transforms
 
 import mlflow
@@ -5,23 +6,29 @@ import PIL
 import torch
 
 
-device = torch.device("mps") # Change this with your device ("cpu", "cuda", "mps")
+app = FastAPI()
 
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
 
-# Load the image
-image = PIL.Image.open("data/test_image.jpg")
+device = torch.device("mps") # Change this with your device ("cpu", "cuda", "mps")
 
-# Preprocess the image
-preprocessing=transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.1307,), (0.3081,))
+@app.post("/predict")
+async def predict(file: UploadFile):
+    # Load the image
+    image = PIL.Image.open(file.file)
+
+    # Transform the image
+
+    # Preprocess the image
+    preprocessing = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
     ])
-preprocessed_image = preprocessing(image)
+    preprocessed_image = preprocessing(image)
 
-loaded_model = mlflow.pytorch.load_model("models:/MyModel@champion")
+    loaded_model = mlflow.pytorch.load_model("models:/MyModel@champion")
 
-predicted_probabilities = loaded_model(preprocessed_image.unsqueeze(0).to(device))
-predicted_label = predicted_probabilities.argmax().cpu().numpy()
+    predicted_probabilities = loaded_model(preprocessed_image.unsqueeze(0).to(device))
+    predicted_label = int(predicted_probabilities.argmax().cpu())
 
-print(predicted_label)
+    return {"prediction": predicted_label}
